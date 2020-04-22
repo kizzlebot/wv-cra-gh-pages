f2a7ba44a01da16bb17654d0070f2472d4a8e031
James <jameschoi@enotarylog.com>
2020-04-22 16:31:26 -0400

    componenets

--- src/components/webviewer.js
+++ src/components/webviewer.js
@@ -6,7 +6,7 @@ import _ from 'lodash';
 class Webviewer extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
     this.viewerRef = React.createRef();
   }
 
@@ -14,35 +14,97 @@ class Webviewer extends Component {
   componentDidMount = async () => {
     const { default: initWv } = await import('@pdftron/webviewer');
 
-    const instance = await initWv({ 
-      ...this.props.config 
+    const instance = await initWv({
+      ...this.props.config,
+      path: '/lib'
     }, this.viewerRef.current);
 
-    
+
     this.instance = instance;
+    const { docViewer, } = instance;
+    window.instance = instance;
     await this.setState({ instance });
+
+
+
+
+    // instance.annotationPopup.add( {
+    //   type: 'statefulButton',
+    //   initialState: 'Page',
+    //   states: {
+    //     Page: {
+    //       // Checkout https://www.pdftron.com/api/web/WebViewerInstance.html to see more APIs related with viewerInstance
+    //       getContent: instance.getCurrentPageNumber,
+    //       onClick: () => {
+    //         const currentPage = instance.getCurrentPageNumber();
+    //         const totalPages = instance.getPageCount();
+    //         const atLastPage = currentPage === totalPages;
+
+    //         if (atLastPage) {
+    //           instance.goToFirstPage();
+    //         } else {
+    //           instance.goToNextPage();
+    //         }
+    //       }
+    //     }
+    //   },
+    //   mount: update => {
+    //     // Checkout https://www.pdftron.com/api/web/CoreControls.DocumentViewer.html to see more APIs and events with docViewer
+    //     // We want to update this button when page number changes so it can have the correct content;
+    //     instance.docViewer.on('pageNumberUpdated.nextPageButton', update);
+    //   },
+    //   unmount: () => {
+    //     instance.docViewer.off('pageNumberUpdated.nextPageButton')
+    //   },
+    //   dataElement: 'nextPageButton'
+    // });
+
+    instance.annotationPopup.add({
+      type: 'customElement',
+      title: 'Select Signer',
+      render: (...args) => {
+        console.log('args', args);
+        console.log('selected');
+        const annot = _.head(instance.annotManager.getSelectedAnnotations());
+
+        return (
+          <div>
+            <label htmlFor="signer">Signer: </label>
+            <select
+              onChange={() => {
+                console.log('annot', annot);
+              }}
+            >
+              <option value="1">1</option>
+              <option value="1">2</option>
+            </select>
+          </div>
+        )
+      }
+    })
+
   }
 
   componentDidUpdate = async (prevProps, prevState) => {
-    if (prevProps.selectedDoc !== this.props.selectedDoc){
-      if (!_.isEmpty(this.props.selectedDoc)){
+    if (prevProps.selectedDoc !== this.props.selectedDoc) {
+      if (!_.isEmpty(this.props.selectedDoc)) {
         await this.instance.loadDocument(this.props.selectedDoc, { l: this.props.config.l, extension: 'pdf' });
       }
     }
   }
-  
-  
 
-  render() { 
-    return (  
+
+
+  render() {
+    return (
       <>
-        <div 
-          style={{ height: '100%' }} 
-          ref={this.viewerRef} 
+        <div
+          style={{ height: '100%' }}
+          ref={this.viewerRef}
         />
       </>
     );
   }
 }
- 
+
 export default Webviewer;
--- src/components/webviewer.stories.js
+++ src/components/webviewer.stories.js
@@ -27,10 +27,40 @@ export const standard = () => {
   const pdftronServer = text('pdftronServer', `https://webviewer-server.staging.enotarylog.com`, 'config');
 
   return (
-    <Webviewer 
+    <Webviewer
       config={{
         l,
         path: `${process.env.PUBLIC_URL}/lib`,
+        fullAPI,
+        pdftronServer,
+        initialDoc,
+        config: config ? `${process.env.PUBLIC_URL}${config}` : undefined
+      }}
+      selectedDoc={selectedDoc}
+    />
+  );
+}
+
+export const beta = () => {
+  const l = text('license', 'eNotaryLog, LLC(enotarylog.com):OEM:eNotaryLog::B+:AMS(20201230):76A52CDD0477580A3360B13AC982537860612F83FF486E45958D86734C8F4E902A4935F5C7', 'config');
+  const fullAPI = boolean('fullApi', true, 'config');
+  const config = text('config', '/config.js', 'config');
+  const initialDoc = text('initialDoc', `https://storage.googleapis.com/enl-static-files/local/linearized.pdf`, 'config');
+
+  const selectedDoc = select('documents', {
+    'linearized.pdf': `https://storage.googleapis.com/enl-static-files/local/linearized.pdf`,
+    'ack.pdf': 'https://storage.googleapis.com/enl-static-files/local/ack.pdf',
+    None: null
+  }, 'linearized.pdf', 'props');
+
+  const pdftronServer = text('pdftronServer', `https://webviewer-server.staging.enotarylog.com`, 'config');
+
+  return (
+    <Webviewer
+      config={{
+        l,
+        path: `${process.env.PUBLIC_URL}/lib`,
+        ui: 'beta',
         fullAPI,
         pdftronServer,
         initialDoc,
